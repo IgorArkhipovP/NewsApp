90513918696e313e7458ae89030287ec7a28ea53
Igor <igor.s.arkhipov@gmail.com>
2022-01-18 10:42:48 +0100

    App is connected to API, images and headlines are displayed on a screen

--- NewsApp/NewsApp/ArticleCell.swift
+++ NewsApp/NewsApp/ArticleCell.swift
@@ -6,14 +6,26 @@
 //
 
 import UIKit
+import Kingfisher
 
 class ArticleCell: UICollectionViewCell {
     
     @IBOutlet weak var articleImageView: UIImageView!
     @IBOutlet weak var headlineLabel: UILabel!
     
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        headlineLabel.text?.removeAll()
+        articleImageView.image = nil
+    }
+    
     func populate(with article: Article){
         headlineLabel.text = article.headline
+        
+        if let urlToImage = article.urlToImage {
+        let url = URL(string: urlToImage)
+        articleImageView.kf.setImage(with: url)
+        }
     }
     
 }
--- NewsApp/NewsApp/NewsCVC.swift
+++ NewsApp/NewsApp/NewsCVC.swift
@@ -6,39 +6,72 @@
 //
 
 import UIKit
+import Alamofire
+import Kingfisher
+
+//country = us
+//apiKey = 2b682086a9b94d598d02712a665d045a
 
 class NewsCVC: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
-    var articles = [Article(headline: "this is news"),
-                    Article(headline: "this is also news"),
-                    Article(headline: "this is super uper great")]
+    var articles = [Article]()
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        DispatchQueue.main.async { [weak self] in
+            self?.getArticles()
+        }
+    
     }
     
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return articles.count
+    func getArticles() {
+        
+        let parameters: Parameters = ["country": "us",
+                                      "apiKey" : "e9bf73fbbdfa4cee9a30ceb59db4accb"]
+        
+        AF.request("https://newsapi.org/v2/top-headlines", parameters: parameters).responseData { [weak self] (response) in
+            
+            guard let data = response.data else { return }
+            
+            do {
+//                let json = try JSONSerialization.jsonObject(with: data, options: [])
+//                print(json)
+                
+                let topHeadlinesResponse = try JSONDecoder().decode(TopHeadlinesResponse.self, from: data)
+                self?.articles = topHeadlinesResponse.articles
+                self?.collectionView?.reloadData()
+                
+            } catch {
+                print(error)
+            }
+            
+        }
+        
     }
-    
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ArticleCell", for: indexPath) as? ArticleCell else { return UICollectionViewCell() }
-        
-        let article = articles[indexPath.item]
-        cell.populate(with: article)
-        
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-        let height: CGFloat = 240
-        let width: CGFloat = (collectionView.frame.width / 2) - 2
-        
-        let size = CGSize(width: width, height: height)
-        
-        return size
-    }
-    
-    
-}
+                                                            
+            override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+                return articles.count
+            }
+            
+            override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+                guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ArticleCell", for: indexPath) as? ArticleCell else { return UICollectionViewCell() }
+                
+                let article = articles[indexPath.item]
+                cell.populate(with: article)
+                
+                return cell
+            }
+            
+            func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+                
+                let height: CGFloat = 240
+                let width: CGFloat = (collectionView.frame.width / 2) - 2
+                
+                let size = CGSize(width: width, height: height)
+                
+                return size
+            }
+        }
+
